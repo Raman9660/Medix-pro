a8ece30db91148dc30e8862ac9861236f268957a
Harsh Vardhan Raj <110082262+Raman9660@users.noreply.github.com>
2025-10-30 15:58:57 +0530

    Update messageRouter.js

--- BACKEND/router/messageRouter.js
+++ BACKEND/router/messageRouter.js
@@ -7,7 +7,26 @@ import { isAdminAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/send", sendMessage);
-// only admin can see the all messages..........
-router.get("/getall", isAdminAuthenticated, getAllMessages);
+router.get("/getall", isAuthenticatedAndAuthorized("admin"), getAllMessages);
 
 export default router;
+
+
+
+
+
+
+
+// import express from "express";
+// import {
+//   getAllMessages,
+//   sendMessage,
+// } from "../controller/messageController.js";
+// import { isAdminAuthenticated } from "../middlewares/auth.js";
+// const router = express.Router();
+
+// router.post("/send", sendMessage);
+// // only admin can see the all messages..........
+// router.get("/getall", isAdminAuthenticated, getAllMessages);
+
+// export default router;
