c4782ac57aa4ca059522f6e9606bdcbab9ee1a40
Harsh Vardhan Raj <110082262+Raman9660@users.noreply.github.com>
2025-10-30 15:57:55 +0530

    Update appointmentRouter.js

--- BACKEND/router/appointmentRouter.js
+++ BACKEND/router/appointmentRouter.js
@@ -12,10 +12,31 @@ import {
 
 const router = express.Router();
 
-router.post("/post", isPatientAuthenticated, postAppointment);
-// only doctor and admin are allowed to get .......................
-router.get("/getall", isAdminAuthenticated, getAllAppointments);
-router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
-router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
+router.post("/post", isAuthenticatedAndAuthorized("patient"), postAppointment);
+router.get("/getall", isAuthenticatedAndAuthorized("admin"), getAllAppointments);
+router.put("/update/:id", isAuthenticatedAndAuthorized("admin"), updateAppointmentStatus);
+router.delete("/delete/:id",isAuthenticatedAndAuthorized("admin"), deleteAppointment);
 
 export default router;
+
+// import express from "express";
+// import {
+//   deleteAppointment,
+//   getAllAppointments,
+//   postAppointment,
+//   updateAppointmentStatus,
+// } from "../controller/appointmentController.js";
+// import {
+//   isAdminAuthenticated,
+//   isPatientAuthenticated,
+// } from "../middlewares/auth.js";
+
+// const router = express.Router();
+
+// router.post("/post", isPatientAuthenticated, postAppointment);
+// // only doctor and admin are allowed to get .......................
+// router.get("/getall", isAdminAuthenticated, getAllAppointments);
+// router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
+// router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
+
+// export default router;
